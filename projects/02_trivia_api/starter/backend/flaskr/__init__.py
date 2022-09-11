a0533f9018ac399df4cd47da27e5e58f8a95bd80
Frank Akrasi Antwi <fantwi@ucc.edu.gh>
2022-09-11 23:41:11 +0000

    Add error handlers for expected errors

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -200,6 +200,38 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          'success': False,
+          'error': 400,
+          'message': 'Bad request'
+      }), 400
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          'success': False,
+          'error': 404,
+          'message': 'Resourse not found'
+      }), 404
+
+  @app.errorhandler(405)
+  def method_not_found(error):
+      return jsonify({
+          'success': False,
+          'error': 405,
+          'message': 'Method not found'
+      }), 405
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          'success': False,
+          'error': 422,
+          'message': 'Unprocessable entity'
+      }), 422
   
   return app
 
